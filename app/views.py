28b1c41cf715f7f10e0d32652c61af57a2f70b29
GouthamiGurramkonda <gouthami487@gmail.com>
2023-06-24 22:03:57 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from app.models import *
+from app.serializers import *
+from rest_framework import viewsets
+class ProductCrudeMVS(viewsets.ModelViewSet):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
